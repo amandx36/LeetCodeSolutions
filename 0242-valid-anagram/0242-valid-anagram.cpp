0057c217e77c6b72f52c7f650cab369923a71b51
Aman Deep <xamandeep360@gmail.com>
2025-12-13 17:05:51 +0530

    Time: 7 ms (20.33%), Space: 9.9 MB (25.26%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false ;
-        unordered_map <char,int> map1 ;
-        for(int i = 0 ; i < s.length(); i++){
-            map1[s[i]]++;
 
-        }
+       
+        if (s.length() != t.length())
+            return false;
 
-        // modifying the map accouring to the second stack 
-        for (int i  = 0 ; i < t.length() ;  i++){
-            char ch  =  t[i];
-            if(map1.find(ch) != map1.end()) {
-                map1[ch]-- ;
-                if(map1[ch]==0) map1.erase(ch);
-            }
-            else {
-                return false ;
-            }
-        }
-        if (map1.size()>0) return false ;
-        return true ;
+        unordered_map<char, int> freq;
+
         
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];    
+            freq[ch] = freq[ch] + 1; 
+        }
+
+       
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t[i];
+
+            
+            if (freq.find(ch) == freq.end())
+                return false;
+
+            freq[ch] = freq[ch] - 1;
+
+            
+            if (freq[ch] == 0)
+                freq.erase(ch);
+        }
+
+       if (freq.size()>0) return false ;
+        return freq.empty();
     }
 };
