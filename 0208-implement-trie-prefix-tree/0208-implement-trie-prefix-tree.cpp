f31e71272930a9f29e386492d9bcff0032c0def7
Aman Deep <xamandeep360@gmail.com>
2025-12-15 14:45:08 +0530

    Time: 51 ms (9.17%), Space: 49.7 MB (84.33%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,53 @@
+class Trie {
+private:
+    struct Node {
+        char data;
+        unordered_map<char, Node*> children;
+        bool isTerminal;
+
+        Node(char ch) {
+            data = ch;
+            isTerminal = false;
+        }
+    };
+
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node('\0');
+    }
+
+    void insert(string word) {
+        Node* curr = root;
+        for (char ch : word) {
+            if (curr->children.count(ch) == 0) {
+                curr->children[ch] = new Node(ch);
+            }
+            curr = curr->children[ch];
+        }
+        curr->isTerminal = true;
+    }
+
+    bool search(string word) {
+        Node* curr = root;
+        for (char ch : word) {
+            if (curr->children.count(ch) == 0) {
+                return false;
+            }
+            curr = curr->children[ch];
+        }
+        return curr->isTerminal;
+    }
+
+    bool startsWith(string prefix) {   // 👈 LeetCode exact name
+        Node* curr = root;
+        for (char ch : prefix) {
+            if (curr->children.count(ch) == 0) {
+                return false;
+            }
+            curr = curr->children[ch];
+        }
+        return true;
+    }
+};
