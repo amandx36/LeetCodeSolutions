2d0993406788a675cd2ad7be3ce179786e64b9c1
Aman Deep <xamandeep360@gmail.com>
2026-01-23 17:25:46 +0530

    Time: 3 ms (19.82%), Space: 21.9 MB (37.08%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        // TC    nlogn  
+        int n  = nums.size();
+        sort(nums.begin(),nums.end());
+        for (int i = 0 ; i < n ; i++){
+            if(nums[i]!=i) return i ;
+        }
+        return n ;
+        
+    }
+};
