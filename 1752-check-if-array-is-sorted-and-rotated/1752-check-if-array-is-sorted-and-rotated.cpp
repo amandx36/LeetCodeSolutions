e720109c9ffd5b0c4d29b05ba6fa5deeba4ba506
Aman Deep <xamandeep360@gmail.com>
2026-05-23 09:48:37 +0530

    Time: 3 ms (2.6%), Space: 16 MB (5.2%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int count = 0 ;
-        for(int i = 0 ; i < nums.size()-1 ;i++){
-            if(nums[i]>nums[i+1]) count++ ;
-            // agar array rotate hua hai to sirf 1 barr  nums[i]>nums[i+1] ye vllid hogi nahi to unsortedd array hogi 
-           
+
+    // function for checking is the sorted 
+    bool isSorted(vector<int>temp){
+       
+        for(int i = 0 ; i < temp.size()-1;i++){
+            if(temp[i]>temp[i+1]) return false ;
+
         }
-        // Circular check: last element agar first element se bada hai toh break count badhao kyu ke ek barr bhi rotate ko sakta hai 
-        if(nums[0]<nums[nums.size()-1]) count++;
-         if(count>=2) return false;
         return true ;
+    }
+
+
+    bool check(vector<int>& nums) {
+
+        // Algo 
+        // rotate and every  rotated is this sort or not 
+        int n = nums.size();
+        for(int i = 0 ; i < n;i++){
+            vector<int> hanged ;
+            for(int  j = 0 ; j < n;j++){
+                hanged.push_back(nums[(i+j)%n]);
+            }
+            if(isSorted(hanged)) return true ;
+        }
+        return false ;
+       
         
     }
 };
