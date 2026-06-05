9d6d5efcdc73cc21de8c8ac7f3f2e1d9cab29d5f
Aman Deep <xamandeep360@gmail.com>
2026-06-05 06:51:14 +0530

    Time: 3 ms (9.77%), Space: 11.7 MB (49.74%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> ans(n, 0);
+        if (k == 0)
+            return ans;
+
+        for (int i = 0; i < n; i++) {
+            int addition = 0;
+            if (k > 0) {
+                for (int j = 1; j <= k; j++) {
+                    addition += code[(i + j) % n];
+                }
+            } else {
+                for (int j = 1; j <= -k; j++) {
+                    addition += code[(i - j + n) % n];
+                }
+            }
+            ans[i] = addition ;
+        }
+        return ans;
+    }
+};
