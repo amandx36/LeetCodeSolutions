ee51ff0bf73751b61dea06475031daa9a470dc66
Aman Deep <xamandeep360@gmail.com>
2026-06-06 21:09:14 +0530

    Time: 0 ms (100%), Space: 10.7 MB (37%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -11,30 +11,38 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        vector<int>bt;
-        ListNode* dummy = head ;
-        while(dummy != nullptr){
-            int val = dummy->val;
-            bt.push_back(val);
-            dummy =  dummy->next ;
-        }
-        int pwT= 2 ;
-        int n = bt.size();
-        vector<int>base ;
-        base.push_back(1);
-        for(int i  = 1  ; i < n ; i++){
+        // vector<int>bt;
+        // ListNode* dummy = head ;
+        // while(dummy != nullptr){
+        //     int val = dummy->val;
+        //     bt.push_back(val);
+        //     dummy =  dummy->next ;
+        // }
+        // int pwT= 2 ;
+        // int n = bt.size();
+        // vector<int>base ;
+        // base.push_back(1);
+        // for(int i  = 1  ; i < n ; i++){
            
-            base.push_back(pwT);
-             pwT *= 2 ;
+        //     base.push_back(pwT);
+        //      pwT *= 2 ;
 
-        }
-        int digit = 0 ;
-        int j = 0 ;
-        for(int i = n - 1 ; i >= 0 ; i--){
-            digit += base[i] * bt[j];
-            j++;
-        }
-        return digit ;
-        
+        // }
+        // int digit = 0 ;
+        // int j = 0 ;
+        // for(int i = n - 1 ; i >= 0 ; i--){
+        //     digit += base[i] * bt[j];
+        //     j++;
+        // }
+        // return digit ;
+
+    int ans = 0 ;
+    while(head){
+        ans = ans*2 + head->val;
+        head = head->next;
+    }
+    return ans ;
+
+
     }
 };
