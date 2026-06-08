7dd5145aa44729fdbcae7289ce8d09a1cd29a192
Aman Deep <xamandeep360@gmail.com>
2026-06-08 16:36:04 +0530

    Time: 0 ms (100%), Space: 8.2 MB (72.38%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -5,10 +5,10 @@ public:
         
         for(int i = 0 ; i < 32 ; i++){
             int lastBit = 0 ;
-            ans = (ans<<1) ;
-               lastBit =  (n&1) ;
+            ans = (ans<<1) ;  // shift left  
+               lastBit =  (n&1) ; // take out the last bit 
                ans = (ans | lastBit) ;
-               n = n>>1;
+               n = n>>1;  // right shift 
         }
         return ans ;
 
