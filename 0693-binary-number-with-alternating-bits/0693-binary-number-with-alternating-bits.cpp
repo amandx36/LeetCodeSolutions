7d2a568f33d31a1c69865efbb3e7a416954eb52c
Aman Deep <xamandeep360@gmail.com>
2026-06-12 08:13:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (27.6%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
+            // TC =  n SC = 1
+        // // total set bit 
+        // int temp = n;
+        // int count = 0;
+        // while (temp != 0) {
+        //     count++;
+        //     temp = temp >> 1;
+        // }
+        // // making the bit
+        // int p1 = 0; // 1010
+        // int p2 = 0; // 0101
 
-        // total set bit 
-        int temp = n;
-        int count = 0;
-        while (temp != 0) {
-            count++;
-            temp = temp >> 1;
-        }
-        // making the bit
-        int p1 = 0; // 1010
-        int p2 = 0; // 0101
+        // for (int i = 0; i < count; i++) {
 
-        for (int i = 0; i < count; i++) {
+        //     p1 <<= 1;
+        //     p2 <<= 1;
 
-            p1 <<= 1;
-            p2 <<= 1;
+        //     if (i % 2 == 0) {
+        //         p1 = p1 | 1;
+        //     } else {
+        //         p2 = p2 | 1;
+        //     }
+        // }
 
-            if (i % 2 == 0) {
-                p1 = p1 | 1;
-            } else {
-                p2 = p2 | 1;
-            }
-        }
+        // return (n == p1) || (n == p2);
 
-        return (n == p1) || (n == p2);
+
+        // // The optimal one is this 
+
+        // TC 1 sc 1 
+        long long  x = n ^ (n >> 1);
+
+        return (x & (x + 1)) == 0;
+
     }
 };
