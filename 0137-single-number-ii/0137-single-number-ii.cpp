f49642ed6de7373602709b5eba3b2fa7be8d5f24
Aman Deep <xamandeep360@gmail.com>
2026-06-21 13:27:59 +0530

    Time: 3 ms (30.61%), Space: 14.1 MB (10.43%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        for(int i = 0  ; i < n;i++){
+            mp[nums[i]]++;
+        }
+        for (auto ele : mp){
+            if(ele.second==1) return ele.first;
+        }
+        return nums[n-1];
+    }
+
+};
