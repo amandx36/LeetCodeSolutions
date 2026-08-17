94c560f0f231ca3ca794560716c6f100ebbe4280
Aman Deep <xamandeep360@gmail.com>
2026-08-17 05:42:04 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.69%) - LeetHub

--- 0942-di-string-match/0942-di-string-match.cpp
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
     vector<int> diStringMatch(string s) {
-        int  n = s.length();
-        vector<int>digi;
-        for(int i = 0 ; i <= n ; i++){
-            digi.push_back(i);
-        }
-        int left = 0 ;
-        int right = n;
-        vector<int>ans ;
-        for(int i  = 0 ; i < n ; i++){
-            if(s[i]=='I'){
-                ans.push_back(digi[left]);
-                digi[left]=-1;
-                left++;
+        // int  n = s.length();
+        // vector<int>digi;
+        // for(int i = 0 ; i <= n ; i++){
+        //     digi.push_back(i);
+        // }
+        // int left = 0 ;
+        // int right = n;
+        // vector<int>ans ;
+        // for(int i  = 0 ; i < n ; i++){
+        //     if(s[i]=='I'){
+        //         ans.push_back(digi[left]);
+        //         digi[left]=-1;
+        //         left++;
+        //     }
+        //     if(s[i]=='D'){
+        //         ans.push_back(digi[right]);
+        //         digi[right]=-1;
+        //         right--;
+        //     }
+
+        // }
+        // for(int i = 0 ; i <=n ; i++){
+        //     if(digi[i]!=-1){
+        //         ans.push_back(digi[i]);
+        //     }
+        // }
+        // return ans;
+        int n = s.length();
+        int left = 0, right = n;
+        vector<int> ans;
+
+        for (char c : s) {
+            if (c == 'I') {
+                ans.push_back(left++);
+            } else {
+                ans.push_back(right--);
             }
-            if(s[i]=='D'){
-                ans.push_back(digi[right]);
-                digi[right]=-1;
-                right--;
-            }
-            
         }
-        for(int i = 0 ; i <=n ; i++){
-            if(digi[i]!=-1){
-                ans.push_back(digi[i]);
-            }
-        }
+
+        ans.push_back(left);
+
         return ans;
     }
 };
