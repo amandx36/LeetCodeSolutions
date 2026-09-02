3d00cf4a473b46989dd936926fdfa3b2ae78e91e
Aman Deep <xamandeep360@gmail.com>
2026-09-02 19:13:15 +0530

    Time: 0 ms (100%), Space: 9 MB (18.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,61 +1,21 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        if (s.size() == 1)
-            return false;
-        stack<char> st;
-        int length = s.size();
-        for (int i = 0; i < length; i++) {
-            if (s[i] == '(') {
-                st.push('(');
-            }
-           else  if (s[i] == '{') {
-                st.push('{');
-            }
-          else   if (s[i] == '[') {
-                st.push('[');
-            }
-          else   if (s[i] == ')') {
-                if (st.empty()) {
-                    return false;
-                }
-                if(st.top()!='('){
-                    return false;
-                }
-                if (st.top() == '(') {
+    bool isValid(string ele) {
+        stack<char>st;
+        for(auto ele : ele){
+            if(ele=='(' || ele=='{' || ele=='[') st.push(ele);
+            else {
+                if(st.empty()) return false ;
+                else {
+                    if((st.top()=='(' and ele!=')') 
+                    || ((st.top()=='{' and ele!='}'))
+                    ||  ((st.top()=='[' and ele!=']'))
+                    )  return false ;
                     st.pop();
-                    continue;
-                }
-                
-            }
-         else    if (s[i] == '}') {
-                if (st.empty()) {
-                    return false;
-                }
-                if (st.top() == '{') {
-                    st.pop();
-                    continue;
-                }
-                if(st.top()!='{'){
-                    return false;
-                }
-            }
-       else      if (s[i] == ']') {
-                if (st.empty()) {
-                    return false;
-                }
-                if (st.top() == '[') {
-                    st.pop();
-                    continue;
-                }
-                if(st.top()!='['){
-                    return false;
                 }
             }
         }
-       if (st.size() == 0)
-            return true;
-
-        return false;
+       if(st.empty()) return true ;
+       return false ;
     }
 };
