1b74bb590e5ec1c26776e68447cfca97bbca6ffd
Aman Deep <xamandeep360@gmail.com>
2026-09-07 08:04:25 +0530

    Time: 0 ms (100%), Space: 7.8 MB (80.43%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,17 +1,30 @@
+
+
 class Solution {
 public:
+    bool solve(int n) {
+
+        if (n == 1)
+            return true;
+
+        if (n <= 0 || n % 4 != 0)
+            return false;
+
+        return solve(n / 4);
+    }
+
     bool isPowerOfFour(int n) {
-        if(n<=0 ) return false ;
-        if((n&n-1)!=0) return false  ;
-        int post = 0 ;
-        while(n>1){
-            post++;
-            n = n >>1;
-        }
-    if(post%2==0) return true ;
-    return false ;
-       
-        
-        
+    // method 01 
+    //     if(n<=0 ) return false ;
+    //     if((n&n-1)!=0) return false  ;
+    //     int post = 0 ;
+    //     while(n>1){
+    //         post++;
+    //         n = n >>1;
+    //     }
+    // if(post%2==0) return true ;
+    // return false ;
+    // method 02
+        return solve(n);
     }
 };
