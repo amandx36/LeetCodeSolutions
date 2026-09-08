f8a71ce932ba1ffe4d5b8077438ecc2a13a07524
Aman Deep <xamandeep360@gmail.com>
2026-09-08 07:23:45 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.47%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        if(n<=999) return 0 ;
+        int commas = 0 ;
+       // break into the parts 
+       if(n>999 and n<=999,999) {
+        commas = n - 999;
+       }
+        return commas;
+    }
+};
